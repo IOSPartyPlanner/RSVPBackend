fc18dd991177892598ba5df5de2871b656fd50cb
Anusha Kopparam <una.020@gmail.com>
2017-05-07 13:45:05 -0700

    Function for triggering RSVP email and handling rsvp

--- functions/index.js
+++ functions/index.js
@@ -19,8 +19,6 @@ exports.sendOutInvites = functions.database
 			const eventHostEmail = post.hostEmail
 			var rsvpLink = "https://fir-logindemo-e7938.firebaseapp.com/test.html?eventId="+eventId 
 			var message = "Event " + eventName + " is being hosted on "+ eventTime + " at " + eventLocation + " Dont forget to rsvp in the link below :)"
-			//console.log(message)
-			//console.log(emails)
 
 		for(i in emails){
 			var email = emails[i]
@@ -39,9 +37,9 @@ function sendEmail(email, hostemail, eventname, message, rsvpLink) {
 
   console.log(message , rsvpLink)
   mailOptions.subject = "You are invited to "+ eventname+" !" ;
-  var htmlMsg = "<body>" + message + "<a href="+rsvpLink+">RSVP</a> </body>"
- mailOptions.html= htmlMsg
- return mailTransport.sendMail(mailOptions).then(() => {
+  var htmlMsg = "<body>" + message + "<br /><a href="+rsvpLink+">RSVP</a> </body>"
+  mailOptions.html= htmlMsg
+  return mailTransport.sendMail(mailOptions).then(() => {
     console.log('Invite sent to', email);
   });
 }
