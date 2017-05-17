d70da86d4a6b4e8dbd95d855a8ce84e9777faebe
Anusha Kopparam <una.020@gmail.com>
2017-05-17 00:35:24 -0700

    Fixed the bug of rsvp being sent on photo upload

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,13 @@ const mailTransport = nodemailer.createTransport(
 exports.sendOutInvites = functions.database
 		.ref('event/{eventId}')
 		.onWrite(event => {
+	      // Only edit data when it is first created.
+
+	      // Exit when the data is deleted.
+    	  if (!event.data.exists()) {
+        	return;
+      	  }
+      		
 			const post = event.data.val()
 			const emails = post.guestEmailList
 			const eventId = post.id
@@ -21,6 +28,31 @@ exports.sendOutInvites = functions.database
 			var rsvpLink = "https://fir-logindemo-e7938.firebaseapp.com/test.html?eventId="+eventId 
 			var message = "Event " + eventName + " is being hosted on "+ eventTime + " at " + eventLocation + " Dont forget to rsvp in the link below :)"
 
+    	  if (event.data.previous.exists()) {
+//     	  const original = event.data.val();
+//     	  const originalGuestList = original.guestEmailList;
+// 		  const previous = event.data.previous.val();
+// 		  const previousGuestList = previous.guestEmailList;
+// 		 
+// 		  console.log("Current data:", originalGuestList.length);
+// 		  console.log("Previous data:", previousGuestList.length);
+// 		  if (originalGuestList.length < previousGuestList.length){
+// 		  	console.log("Email deletion");
+// 		 	return;
+// 		  } 	
+// 		  const diffEmails = symmetricDifference(originalGuestList, previousGuestList);
+// 		  if (diffEmails.length > 0){ 
+//           	for(i in diffEmails){
+// 				var email = diffEmails[i]
+// 				console.log("sending emails to: ", email)
+// 				rsvpLink = rsvpLink + "&guestEmail="+email+"&eventName="+eventName+"&eventLocation="+eventLocation+"&eventTime="+eventTime
+// // 				rsvpAppLink = rsvpAppLink + "&guestEmail="+email+"&eventName="+eventName+"&eventLocation="+eventLocation+"&eventTime="+eventTime
+// 				sendEmail(email, eventHostEmail, eventName, message, rsvpLink);//, rsvpAppLink)
+// 			}
+// 			return;
+//           }		
+			return;
+      	}
 		for(i in emails){
 			var email = emails[i]
 			console.log("sending emails to: ", email)
@@ -31,6 +63,21 @@ exports.sendOutInvites = functions.database
 		});
 
 
+function symmetricDifference(a1, a2) {
+  var result = [];
+  for (var i = 0; i < a1.length; i++) {
+    if (a2.indexOf(a1[i]) === -1) {
+      result.push(a1[i]);
+    }
+  }
+  for (i = 0; i < a2.length; i++) {
+    if (a1.indexOf(a2[i]) === -1) {
+      result.push(a2[i]);
+    }
+  }
+  return result;
+}
+ 
 function sendEmail(email, hostemail, eventname, message, rsvpLink){//, rsvpAppLink) {
   const mailOptions = {
     from: "Party planners "+ hostemail,
